c328c8ae0000d1c41bee1c3b08a0c7d6ccd848b2
maks <vlamaster08@gmail.com>
2021-11-06 13:39:54 +0300

    Task1

--- sources/m_stack2.cpp
+++ sources/m_stack2.cpp
@@ -1,9 +1,6 @@
 // Copyright 2021 Max
-
 #include "m_stack2.hpp"
 
-#include <stdexcept>
-
-auto example() -> void {
-  throw std::runtime_error("not implemented");
+void example()  {
+  return;
 }
--- tests/example.cpp
+++ tests/example.cpp
@@ -6,10 +6,27 @@
 #include <m_stack2.hpp>
 #include <stdexcept>
 
+class NonCopEx {
+  double data;
+
+ public:
+  NonCopEx() = default;
+  NonCopEx(const NonCopEx&) = delete;
+  NonCopEx(NonCopEx&&) = default;
+  NonCopEx(double _data) :data (_data){
+  }
+  ~NonCopEx() = default;
+  auto operator=(NonCopEx&&) -> NonCopEx& = default;
+  auto operator=(const NonCopEx&) -> NonCopEx& = delete;
+  double getData() const { return this->data; }
+};
+
 //--------Testing Stack1 ---------------
 
-TEST(Example, EmptyTest) {
-  EXPECT_TRUE(true);
+TEST(Stack1, Empty) {
+  Stack1<int> stack;
+  EXPECT_THROW(stack.head(),std::runtime_error);
+  EXPECT_THROW(stack.pop(),std::runtime_error);
 }
 
 

d1fb10afd858f5945012d3a03d08ac1cf95e37b7
maks <vlamaster08@gmail.com>
2021-11-06 13:20:53 +0300

    Task1

--- tests/example.cpp
+++ tests/example.cpp
@@ -1,7 +1,17 @@
-// Copyright 2021 Your Name <your_email>
+// Copyright 2021 Max
 
 #include <gtest/gtest.h>
 
 #include <m_stack1.hpp>
 #include <m_stack2.hpp>
 #include <stdexcept>
+
+//--------Testing Stack1 ---------------
+
+
+
+
+
+
+//--------Testing Stack2 ---------------
+
